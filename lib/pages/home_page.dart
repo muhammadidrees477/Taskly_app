6134491a8dd4d9d1591523d08d84d80260078f8a
Muhammad Idrees <midreesktk9@gmail.come>
2025-07-26 14:01:12 +0500

    2nd  commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -38,7 +38,9 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontSize: 25),
         ),
       ),
-      body: Column(children: [ _taskView()]),
+      body: Column(children: [
+        Spacer(),
+        _taskView()]),
       floatingActionButton: _addTaskButton(),
 
     );
